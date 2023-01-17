734ea7e4dc00848660faf571590fcb6d830ea198
saradamonte <sara.damonte@edu.unito.it>
2023-01-17 12:46:21 +0100

    poco e niente

--- Esercizio2_Summarization/summarization.py
+++ Esercizio2_Summarization/summarization.py
@@ -3,3 +3,13 @@ import pandas as pd
 df_nasari = pd.read_csv('Esercizio2_Summarization/nasari/dd-small-nasari-15.txt', sep=';', header=None)
 
 
+def read_and_split(doc):
+    paragraphs = list()
+    document = open(doc, 'r')
+    for line in document:
+        if len(line) > 1:
+            paragraphs.extend(line.split('\n'))
+    document.close()
+
+
+read_and_split('Esercizio2_Summarization/utils/Andy-Warhol.txt')
