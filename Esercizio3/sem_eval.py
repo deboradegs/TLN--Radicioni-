8f1d69dcb1acd24f72d581db5f86a164d8256d15
saradamonte <sara.damonte@edu.unito.it>
2023-01-31 00:10:04 +0100

    Direi finito... o QUASI
    Co-authored-by: deboradegs <deboradegs@users.noreply.github.com>

--- Esercizio3/sem_eval.py
+++ Esercizio3/sem_eval.py
@@ -24,8 +24,10 @@ def create_dicts():
 
 create_dicts()
 
-df_scores = pd.read_csv('Esercizio3/chierchiello.csv', sep=',', header=None)
-
+df_scores = pd.read_csv('Esercizio3/words_from_Chierchiello.csv', sep=',', header=None)
+df_damonte = pd.read_csv('Esercizio3/damonte_senses.csv', sep=',', header=None)
+df_degaetano = pd.read_csv('Esercizio3/deGaetano_senses.csv', sep=',', header=None)
+df_chierchiello = pd.read_csv('Esercizio3/chierchiello_senses.csv', sep=',', header=None)
 
 pearson_agreement = (df_scores[2].corr(df_scores[3], method='pearson')+ \
             df_scores[2].corr(df_scores[4], method='pearson')+ \
@@ -34,27 +36,17 @@ spearman_agreement = (df_scores[2].corr(df_scores[3], method='spearman')+\
             df_scores[2].corr(df_scores[4], method='spearman')+\
             df_scores[3].corr(df_scores[4], method='spearman'))/3
 
-terms = list()
-for rows in df_scores.itertuples():
-    #print(rows)
-    dict_scores = dict.fromkeys(['Term1', 'Term2', 'average_score'])
-    dict_scores['Term1'] = rows[1]
-    dict_scores['Term2'] = rows[2]
-    dict_scores['average_score'] = round((int(rows[3])+int(rows[4])+int(rows[5]))/3, 3)
-    terms.append(dict_scores)
-
-#print(terms)
-
+dict_for_accuracy = dict()
 
 def calculate_cosine_sim():
-    dicts_score_cosine = list()
     list_annotate_score = list()
     list_cosine_sim = list()
-    for dizio in terms:
+    #i=0
+    for row in df_scores.itertuples():
         max_cosine_total = 0
-        if dizio['Term1'] in dict_semVal.keys() and dizio['Term2'] in dict_semVal.keys():
-            babel1 = dict_semVal[dizio['Term1']] 
-            babel2 = dict_semVal[dizio['Term2']]
+        if row[1] in dict_semVal.keys() and row[2] in dict_semVal.keys():
+            babel1 = dict_semVal[row[1]] 
+            babel2 = dict_semVal[row[2]]
             for bn1 in babel1:
                 max_cosine_inter = 0
                 if bn1 in dict_nasari.keys():
@@ -69,40 +61,80 @@ def calculate_cosine_sim():
                 if max_cosine_inter > max_cosine_total:
                     max_cosine_total = max_cosine_inter 
                     max_babel1 = bn1
-        
-            dizio['cosine_sim'] = max_cosine_total
-            dizio['bn1'] = max_babel1
-            dizio['bn2'] = max_babel2
-            #print(dizio)
-            list_annotate_score.append(dizio['average_score'])
+            average_scores = round((int(row[3])+int(row[4])+int(row[5]))/3, 3)
+            list_annotate_score.append(average_scores)
             list_cosine_sim.append(max_cosine_total)
-            dicts_score_cosine.append(dizio)
+            dict_for_accuracy[(row[1], row[2])] = [(max_babel1, max_babel2)]#, (df_chierchiello['bn1'][i], df_chierchiello['bn2'][i])]
+        #i+=1
     df = pd.DataFrame(list(zip(list_annotate_score,list_cosine_sim)), columns = ['Average_Score_Annotate','Cosine_Sim'])
-    return dicts_score_cosine, df
+    return df
 
-dicts_score_cosine = calculate_cosine_sim()[0]
-df_calculate_scores = calculate_cosine_sim()[1]
+df_calculate_scores = calculate_cosine_sim()
 
 pearson_valutation = df_calculate_scores['Average_Score_Annotate'].corr(df_calculate_scores['Cosine_Sim'], method='pearson')
 spearman_valutation = df_calculate_scores['Average_Score_Annotate'].corr(df_calculate_scores['Cosine_Sim'], method='spearman')
 
-print(pearson_valutation)
-print(spearman_valutation)
 
-#print(dicts_score_cosine)
-# list1= [('bn:123456n','bn:124565n'),('bn:567888n','bn:000234n'),('bn:000111n', 'bn:111222n')]
-# list2 = [('bn:349445n', 'bn:124564n'), ('bn:123565n','bn:123665n'), ('bn:128565n','bn:163665n')]
-# df_prova= pd.DataFrame(list(zip(list1,list2)), columns = ['lista1','lista2'])
+#Consegna 1:
+print()
+print("----------------------------------------------------------")
+print("Agreement between annotations")
+print("Pearson coefficient: ", str(pearson_agreement))
+print("Spearman coefficient: ", str(spearman_agreement))
+print("----------------------------------------------------------")
 
-# pearson_valutation_prova= df_prova['lista1'].corr(df_prova['lista2'], method='pearson')
-# spearman_valutation_prova = df_prova['lista1'].corr(df_prova['lista2'], method='spearman')
-# print('pearson_prova')
-# print(pearson_valutation_prova)
+print()
 
-df_damonte = pd.read_csv('Esercizio3/damonte_senses.csv', sep=',', header=None, names=['Term1', 'Term2', 'bn1','bn2'])
-df_degaetano = pd.read_csv('Esercizio3/deGaetano_senses.csv', sep=',', header=None, names=['Term1', 'Term2', 'bn1','bn2'])
+print("----------------------------------------------------------")
+print("Correlation between human and machine")
+print("Pearson coefficient: ", str(pearson_valutation))
+print("Spearman coefficient: ", str(spearman_valutation))
+print()
 
 
-kappa_damonte_degaetano = sklearn.metrics.cohen_kappa_score(np.array(df_damonte['bn1']), np.array(df_degaetano['bn1']))
-print(kappa_damonte_degaetano)
+def evaluation(person):
+    evaluation_couple_words = 0
+    count_match = 0
+    count_couple_match = 0
+    i=0
+    lista_babel = list()
+    for tuple in dict_for_accuracy:
+        #dict_for_accuracy[tuple].append((person['bn1'][i], person['bn2'][i]))
+        if dict_for_accuracy[tuple][0] == (person[2][i], person[3][i]):
+            count_couple_match+=1
+        if dict_for_accuracy[tuple][0][0] == person[2][i]:
+            count_match+=1
+        if dict_for_accuracy[tuple][0][1] == person[3][i]:
+            count_match+=1
+        #dict_for_accuracy[tuple].remove((person['bn1'][i], person['bn2'][i]))
+        lista_babel.append((person[2][i], person[3][i]))
+        i+=1
+    evaluation_single_word = count_match/(len(dict_for_accuracy)*2)
+    evaluation_couple_words = count_couple_match/len(dict_for_accuracy)
+    return evaluation_single_word, evaluation_couple_words, lista_babel
+    
 
+kappa_damonte_degaetano = sklearn.metrics.cohen_kappa_score(np.reshape(evaluation(df_damonte)[2], -1), np.reshape(evaluation(df_degaetano)[2], -1))
+kappa_damonte_chierchiello = sklearn.metrics.cohen_kappa_score(np.reshape(evaluation(df_damonte)[2], -1), np.reshape(evaluation(df_chierchiello)[2], -1))
+kappa_deGaetano_chierchiello = sklearn.metrics.cohen_kappa_score(np.reshape(evaluation(df_degaetano)[2], -1), np.reshape(evaluation(df_chierchiello)[2], -1))
+
+
+#Consegna 2:
+print()
+print("----------------------------------------------------------")
+print("Agreement between annotations with Cohen similarity:")
+print("Cohen similarity: ", str((kappa_damonte_degaetano+kappa_damonte_chierchiello+kappa_deGaetano_chierchiello)/3))
+print("----------------------------------------------------------")
+
+print()
+
+
+print("----------------------------------------------------------")
+print("Accuracy for single word (Chierchiello): {}".format(evaluation(df_chierchiello)[0]))
+print("Accuracy for single word (Damonte): {}".format(evaluation(df_damonte)[0]))
+print("Accuracy for single word (De Gaetano): {}".format(evaluation(df_degaetano)[0]))
+
+print("\nAccuracy for pair word (Chierchiello): {}".format(evaluation(df_chierchiello)[1]))
+print("Accuracy for pair word (Damonte): {}".format(evaluation(df_damonte)[1]))
+print("Accuracy for pair word (De Gaetano): {}".format(evaluation(df_degaetano)[1]))
+print()
